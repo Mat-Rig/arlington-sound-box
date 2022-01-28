0cf16479e9aaa93eddb3a657d9924a7553409827
mathias.rigaud <mathias.rigaud@rainbird.ai>
2022-01-28 20:31:36 +0000

    version 0 test 3

--- src/App.js
+++ src/App.js
@@ -15,13 +15,17 @@ function App() {
   //be careful
   //good night
   //Luuuuuk
+  //Joder
+  //Que tonta
   let mariaTired  = new Audio("/mariaTired.m4a")
 
 //Tom
   let tomWetwipe = new Audio("/tomWetwipe.m4a")
-  //cry me a river
+  let tomBecareful = new Audio("/tomBecareful.m4a")
+  let tomCrymeariver = new Audio("/tomCrymeariver.m4a")
   //Are you funcking dumb
   //I'm nacked
+  //Lost the plot
   //good night
   //I tell you what
   //Bish Bash Bosh
@@ -30,7 +34,7 @@ function App() {
   //(what's for dinner?) Fish and vegs
   //I believe I can fly
   let pieroLikenotomorrow = new Audio("/pieroLikenotomorrow.m4a");
-  //hey maan!
+  let pieroHeyMan = new Audio("/pieroHeyMan.m4a");
   //Aaahhh euuh
   //good night
   //oh yes man, only the best
@@ -38,14 +42,15 @@ function App() {
   //The problem is...
 
 //Mathias
-  //Sometaaaimes
+  let mathiasSometimes = new Audio("/mathiasSometimes.m4a");
+  //Wocestershire
   //Tommy boy!! what you doing?
   //Naaah ... I don't know
   let mathiasTommyBoy = new Audio("/mathiasTommyBoy.m4a");
   let mathiasSorryWhat = new Audio("/mathias-sorryWhat.m4a") //can't be arsed to cook
   //Goodnight
   //I'm so fucking tired man
-  mathiasSorray
+  let mathiasSorray = new Audio("/mathiasSorray.m4a");
   let mathiasCba = new Audio("/mathiasCba.m4a") //can't be arsed to cook
 
 
@@ -65,8 +70,13 @@ function App() {
         <div className="soundbox" onClick={() => {start(mathiasTommyBoy)}}> Tommy Boy! </div>
         <div className="soundbox" onClick={() => {start(mariaTired)}}> Maria is tired </div>
         <div className="soundbox" onClick={() => {start(mathiasCba)}}> Mathias CBA </div>
+        <div className="soundbox" onClick={() => {start(tomBecareful)}}> Be careful, be careful!</div>
         <div className="soundbox" onClick={() => {start(tomWetwipe)}}> Tom wetwipe </div>
         <div className="soundbox" onClick={() => {start(mathiasSorryWhat)}}> Sorry what?!?</div>
+        <div className="soundbox" onClick={() => {start(mathiasSorray)}}> Sorray! </div>
+        <div className="soundbox" onClick={() => {start(pieroHeyMan)}}> Hey man! </div>
+        <div className="soundbox" onClick={() => {start(mathiasSometimes)}}> Sometimes </div>
+        <div className="soundbox" onClick={() => {start(tomCrymeariver)}}> Cry me a river </div>
       </div>
     </div>
   );
